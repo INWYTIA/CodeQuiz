cd1a3832886d53fdcae74f76aa1fbae6617f8400
INWYTIA <inwytia@gmail.com>
2020-01-06 10:40:10 -0600

    added quiz questions

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        q : "As the framework of a webpage HTML organizes information using what common device?",
+        choices : ["a header", "math", "a tag", "grammar",],
+        a : "a tag",
+    },
+    {
+        q : "CSS provides an aesthetic upgrade to HTML by applying which of the following?",
+        choices : ["color", "borders and spacing", "text styles", "all of the above",],
+        a : "all of the above",
+    },
+    {
+        q : "If a collection of information in javascript can be described in multiple ways, it's probably best to store it as...",
+        choices : ["a variable", "an object", "a function", "an array",],
+        a : "an object",
+    },
+    {
+        q : "Bootstrap is to CSS as jQuery is to...",
+        choices : ["javascript", "CSS", "the internet", "HTML",],
+        a : "javascript",
+    },
+    {
+        q : "Which of the following does not help code to be organized and easy to read?",
+        choices : ["indentation", "line breaks", "comments", "debugging",],
+        a : "debugging",
+    },
+]
