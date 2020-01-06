394ccb5a5b23a02a9eacd98948c42a8eb8cc8d09
INWYTIA <inwytia@gmail.com>
2020-01-06 14:32:16 -0600

    additional javascript to provide cycling questions and answer verification

--- script.js
+++ script.js
@@ -5,16 +5,30 @@ function countDown() {
     timeLeft = timeLeft--;
 }
 
-function nextQ() {
+function answer() {
+    if (this.id === questions[currentQ].a) {
+        currentQ = currentQ++;
+        loadQ();
+    } else {
+        timeLeft = timeLeft - 15;
+        $('p').remove()
+        $('.card-body').append('<p>Incorrect</p>');
+    }
+}
 
+function loadQ() {
+    $('p').remove()
+    $('.card-title').html('Question ' + (currentQ + 1));
+    $('.card-text').html(questions[currentQ].q);
+    $('.btn').remove();
+    $('.card-body').append('<button type="button" id="' + questions[currentQ].choices[0] + '" class="btn btn-primary a-btn">' + questions[currentQ].choices[0] + '</button>', '<button type="button" id="' + questions[currentQ].choices[1] + '" class="btn btn-primary a-btn">' + questions[currentQ].choices[1] + '</button>', '<button type="button" id="' + questions[currentQ].choices[2] + '" class="btn btn-primary a-btn">' + questions[currentQ].choices[2] + '</button>', '<button type="button" id="' + questions[currentQ].choices[3] + '" class="btn btn-primary a-btn">' + questions[currentQ].choices[3] + '</button>');
 }
 
 function quizStart() {
     setInterval(countDown, 1000);
-    $('.card-title').html('Question ' + (currentQ + 1));
-    $('.card-text').html(questions[currentQ].q);
-    $('.btn').remove();
-    $('.card-body').append('<button type="button" class="btn btn-primary">' + questions[currentQ].choices[0] + '</button>', '<button type="button" class="btn btn-primary">' + questions[currentQ].choices[1] + '</button>', '<button type="button" class="btn btn-primary">' + questions[currentQ].choices[2] + '</button>', '<button type="button" class="btn btn-primary">' + questions[currentQ].choices[3] + '</button>');
+    loadQ()
 };
 
 $('.btn-lg').on('click', quizStart());
+
+$('.a-btn').on('click', answer())
